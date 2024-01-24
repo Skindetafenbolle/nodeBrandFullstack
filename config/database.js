04ee6cb658935d0f32ce0626d2b6a5c9d0506f39
Skindetafenbolle <elited013@gmail.com>
2024-01-24 16:37:29 +0300

    update require

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const Sequilize = require('sequelize')
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('postgres://wikyx:Hik9sPSPRkP3Ds8R3o3Hhvc9SuvxUSD4@dpg-cmogsfa1hbls73b5uhjg-a/brandsrb')
 
